7032d1f9fc90265430ef1817d1d90756f10ddd0b
Xoipz <xoipz@follow.re>
2025-04-19 16:20:30 +0800

    更新页面头部组件，添加侧边栏切换功能，支持移动端和桌面端的侧边栏显示状态，优化翻译支持和工具提示展示。

--- apps/client/src/features/page/components/header/page-header.tsx
+++ apps/client/src/features/page/components/header/page-header.tsx
@@ -1,16 +1,60 @@
 import classes from "./page-header.module.css";
 import PageHeaderMenu from "@/features/page/components/header/page-header-menu.tsx";
-import { Group } from "@mantine/core";
+import { Group, Tooltip } from "@mantine/core";
 import Breadcrumb from "@/features/page/components/breadcrumbs/breadcrumb.tsx";
+import { useAtom } from "jotai";
+import {
+  desktopSidebarAtom,
+  headerVisibleAtom,
+  mobileSidebarAtom,
+} from "@/components/layouts/global/hooks/atoms/sidebar-atom.ts";
+import { useToggleSidebar } from "@/components/layouts/global/hooks/hooks/use-toggle-sidebar.ts";
+import SidebarToggle from "@/components/ui/sidebar-toggle-button.tsx";
+import { useTranslation } from "react-i18next";
 
 interface Props {
   readOnly?: boolean;
 }
 export default function PageHeader({ readOnly }: Props) {
+  const { t } = useTranslation();
+  const [mobileOpened] = useAtom(mobileSidebarAtom);
+  const toggleMobile = useToggleSidebar(mobileSidebarAtom);
+
+  const [desktopOpened] = useAtom(desktopSidebarAtom);
+  const toggleDesktop = useToggleSidebar(desktopSidebarAtom);
+
+  const [headerVisible] = useAtom(headerVisibleAtom);
+
   return (
     <div className={classes.header}>
       <Group justify="space-between" h="100%" px="md" wrap="nowrap">
-        <Breadcrumb />
+        <Group wrap="nowrap">
+          {!headerVisible && (
+            <Tooltip label={t("Sidebar toggle")}>
+              <SidebarToggle
+                aria-label={t("Sidebar toggle")}
+                opened={mobileOpened}
+                onClick={toggleMobile}
+                hiddenFrom="sm"
+                size="sm"
+              />
+            </Tooltip>
+          )}
+
+          {!headerVisible && (
+            <Tooltip label={t("Sidebar toggle")}>
+              <SidebarToggle
+                aria-label={t("Sidebar toggle")}
+                opened={desktopOpened}
+                onClick={toggleDesktop}
+                visibleFrom="sm"
+                size="sm"
+              />
+            </Tooltip>
+          )}
+
+          <Breadcrumb />
+        </Group>
 
         <Group justify="flex-end" h="100%" px="md" wrap="nowrap">
           <PageHeaderMenu readOnly={readOnly} />
